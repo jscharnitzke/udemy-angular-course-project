872a1cf9894e8ed76061819346cf78b144a018e4
Joe Scharnitzke <j.scharnitzke@gmail.com>
2018-04-20 10:56:35 -0700

    Refactored components to be more in line with the design used by the instructor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipePanelComponent } from './recipe-panel/recipe-panel.component';
+import { RecipeListComponent } from './recipe-panel/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-panel/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-panel/recipe-list/recipe-item/recipe-item.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ShoppingListComponent,
+    HeaderComponent,
+    RecipePanelComponent,
     RecipeListComponent,
-    HeaderComponent
+    RecipeDetailComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
--- src/app/recipe-list/recipe/recipe-detail/recipe-detail.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-recipe-detail',
-    templateUrl: './recipe-detail.component.html'
-})
-export class RecipeDetailComponent {
-
-}
