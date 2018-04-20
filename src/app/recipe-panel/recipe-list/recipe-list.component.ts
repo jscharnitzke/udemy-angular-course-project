8b1107100d2f0d2fceff7a35dab8f4b5123b4ed4
Joe Scharnitzke <j.scharnitzke@gmail.com>
2018-04-20 11:43:39 -0700

    Added placeholder data for the recipe list

--- src/app/recipe-panel/recipe-list/recipe-list.component.ts
+++ src/app/recipe-panel/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Mac & Cheese',
+      'Old fashioned mac and cheese',
+      'https://images-gmi-pmc.edge-generalmills.com/c41ffe09-8520-4d29-9b4d-c1d63da3fae6.jpg')
+  ];
 
   constructor() { }
 
