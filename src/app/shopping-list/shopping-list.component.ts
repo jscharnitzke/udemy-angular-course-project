e19374e478072b45712b3e9f15a17630320c8982
Joe Scharnitzke <j.scharnitzke@gmail.com>
2018-04-20 11:57:14 -0700

    Added an ingredient model and placeholder data for the shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,6 +8,10 @@ import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-ed
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Block of Cheese', 1),
+    new Ingredient('Bag of Elbow Noodles', 0.5)
+  ];
 
   constructor() { }
 
