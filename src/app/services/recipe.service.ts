3cfc32338ac0bbb97bd648a71ca9717b73b095b7
Joe Scharnitzke <j.scharnitzke@gmail.com>
2018-04-24 16:15:37 -0700

    Implemented ingredients on recipess

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from '../recipe-panel/recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
@@ -8,7 +9,12 @@ export class RecipeService {
         new Recipe(
             'Mac & Cheese',
             'Old fashioned mac and cheese',
-            'https://images-gmi-pmc.edge-generalmills.com/c41ffe09-8520-4d29-9b4d-c1d63da3fae6.jpg')
+            'https://images-gmi-pmc.edge-generalmills.com/c41ffe09-8520-4d29-9b4d-c1d63da3fae6.jpg',
+            [
+                new Ingredient('Cheese', 1),
+                new Ingredient('Bag of Elbow Noodles', 2)
+            ]
+        )
     ];
 
     getRecipes() { return [...this.recipes]; }
