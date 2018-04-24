b07f7d6aaf588aec24fe2ae1bddf34e49bc4ecff
Joe Scharnitzke <j.scharnitzke@gmail.com>
2018-04-24 11:21:13 -0700

    Made shopping list additions more robust

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Output, EventEmitter, ElementRef } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
     selector: 'app-shopping-list-edit',
@@ -6,4 +7,23 @@ import { Component } from '@angular/core';
 })
 
 export class ShoppingListEditComponent {
+    @ViewChild('nameInput') nameInput: ElementRef;
+    @ViewChild('amountInput') amountInput: ElementRef;
+    @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+    onAddClicked() {
+        const ingredientName = this.nameInput.nativeElement.value;
+        const ingredientAmount = this.amountInput.nativeElement.value;
+
+        if (ingredientName.length === 0 || ingredientAmount.length === 0) {
+            return;
+        }
+
+        const newIngredient = new Ingredient(
+            ingredientName,
+            ingredientAmount
+        );
+
+        this.ingredientAdded.emit(newIngredient);
+    }
 }
