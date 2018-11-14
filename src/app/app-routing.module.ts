b6e2db202dba88e3c5194a65b287bbcdb6425d29
Joe Scharnitzke <j.scharnitzke@gmail.com>
2018-11-14 15:34:10 -0800

    recipe routing now uses array index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecipePanelComponent } from './recipe-panel/recipe-panel.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeDetailComponent } from './recipe-panel/recipe-detail/recipe-detail.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', component: RecipePanelComponent },
+  {
+    path: 'recipes',
+    component: RecipePanelComponent,
+    children: [{ path: 'recipes/:recipeId', component: RecipeDetailComponent }]
+  },
   { path: 'list', component: ShoppingListComponent }
 ];
 
